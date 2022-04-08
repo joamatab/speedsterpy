00b0561e1dab8da19f2ec08b8299b88c7df41e07
dasdias <das.dias6@gmail.com>
2022-04-08 15:32:23 +0100

    initial commit - Speedster , python implementation, version 1.2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Speedster - Python Version 1.2
+
+Speedster is a 3D point-to-point parasitic resistance extraction tool for integrated circuits.
---
+++ src/speedster_py/main.py
@@ -0,0 +1,10 @@
+from loguru import logger
+import os
+
+def main():
+    print("Speedster, Version 1.2")
+    logger.info("Project File: {}".format(os.path.abspath(__file__)))
+
+
+if __name__ == "__main__":
+    main()
